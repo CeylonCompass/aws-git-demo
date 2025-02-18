67f9af660a01ec768b5e2509030dbcd67e82e9c0
Navindu Kavishka <navindu.kavishka@helixzsolutions.com>
2025-02-18 12:47:13 +0530

    feat: -[HELIXZ-0001] basic api endpoints created

--- src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
@@ -1,10 +1,32 @@
 package com.helixz.awsgitdemo.messages;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Chamith Kodikara
  */
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/messages")
 public class MessageController {
+
+
+    private final MessageService messageService;
+    @PostMapping
+    public String createMessage(@RequestBody Message message) {
+        messageService.createMessage(message);
+        return "message saved successfully!";
+    }
+
+    @GetMapping
+    public List<Message> getAllMessages() {
+        return messageService.getAllMessages();
+    }
+
+
+
+
 }
--- src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
@@ -1,8 +1,12 @@
 package com.helixz.awsgitdemo.messages;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author Chamith Kodikara
@@ -10,5 +14,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class MessageService {
+
+
+    private final MessageRepository messageRepository;
+
+    public void createMessage(Message message) {
+        message.setCreatedDate(LocalDateTime.now());
+        message.setLastModifiedDate(LocalDateTime.now());
+        messageRepository.save(message);
+    }
+
+    public List<Message> getAllMessages() {
+        return messageRepository.findAllByOrderByCreatedDateDesc();
+    }
 }
