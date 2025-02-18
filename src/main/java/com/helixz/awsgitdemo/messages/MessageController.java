4630b7439db4533926c029bd695c6d6ac73c7087
ChamithChathurya-Helixz <chamith.chathurya@helixzsolutions.com>
2025-02-18 14:20:55 +0530

    test: add unit tests for message service

--- src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
@@ -1,10 +1,29 @@
 package com.helixz.awsgitdemo.messages;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Chamith Kodikara
  */
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class MessageController {
+
+    private final MessageService messageService;
+
+    @PostMapping("/message")
+    public ResponseEntity<Message> createMessage(@RequestBody Message message){
+        return messageService.createMessage(message);
+
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getMessage(){
+        return messageService.getMessage();
+    }
 }
--- src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
@@ -1,8 +1,15 @@
 package com.helixz.awsgitdemo.messages;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 
 /**
  * @author Chamith Kodikara
@@ -10,5 +17,29 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class MessageService {
+
+    private final MessageRepository messageRepository;
+
+    public ResponseEntity<Message> createMessage(Message message) {
+        if (message ==null){
+            log.error("Message is null");
+            return ResponseEntity.badRequest().build();
+        }
+        Message saveMessage = messageRepository.save(message);
+
+        return new ResponseEntity<>(saveMessage,HttpStatus.CREATED);
+
+
+
+    }
+
+    public List<Message> getMessage(){
+        return messageRepository.findAll(Sort.by(Sort.Direction.DESC,"createdDate"));
+    }
+
+
+
+
 }
