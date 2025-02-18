8df5b494f17c804f496c132a5dda5ff02e26b5b9
SupunBHerathHelixz <supun.herath@helixzsolutions.com>
2025-02-18 17:28:12 +0530

    feat: Implement message POST and GET APIs

--- src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
@@ -1,10 +1,30 @@
 package com.helixz.awsgitdemo.messages;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author Chamith Kodikara
  */
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class MessageController {
+
+   private final MessageService messageService;
+
+    @PostMapping("/messages")
+    public ResponseEntity<?> createMessage(@RequestBody Map<String, String>  content) {
+        String messageContent = content.get("content");
+        return messageService.createMessage(messageContent);
+    }
+
+    @GetMapping("/messages")
+    public ResponseEntity<?> getMessages () {
+        return messageService.getMessage();
+    }
 }
--- src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
@@ -1,8 +1,19 @@
 package com.helixz.awsgitdemo.messages;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Chamith Kodikara
@@ -10,5 +21,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class MessageService {
+
+
+    private final MessageRepository messageRepository;
+
+    public ResponseEntity<?> createMessage(String content) {
+        if (content == null || content.trim().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(Collections.singletonMap("error", "Content must not be empty/null"));
+        }
+
+        Message message = new Message();
+        message.setMessage(content);
+        Message savedMessage = messageRepository.save(message);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("id", savedMessage.getId());
+        response.put("content", savedMessage.getMessage());
+        response.put("createdAt", savedMessage.getCreatedDate());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    public ResponseEntity<?> getMessage(){
+        return  ResponseEntity.ok(messageRepository.findAllByOrderByCreatedDateDesc());
+    }
+
 }
