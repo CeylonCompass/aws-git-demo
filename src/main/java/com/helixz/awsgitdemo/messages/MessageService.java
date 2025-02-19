ca92f9d8912c30d95ac37fe84bb46a280d435902
chamith kodikara <chamith@helixzsolutions.com>
2025-02-19 16:29:16 +0100

    feat:[HELIXZ-0001] search api impl -basic search impl

--- src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
@@ -1,5 +1,8 @@
 package com.helixz.awsgitdemo.messages;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +22,16 @@ public class MessageService {
 
     public Message createMessage(Message message) {
         return messageRepository.save(message);
+    }
+
+    public Page<Message> searchMessages(String searchTerm, Pageable pageable) {
+        Page<Message> messages;
+        if (StringUtils.isBlank(searchTerm)) {
+            messages = messageRepository.findAll(pageable);
+        } else {
+            searchTerm = new StringBuilder().append("%").append(searchTerm).append("%").toString();
+            messages = messageRepository.findAll(searchTerm, pageable);
+        }
+        return messages;
     }
 }
