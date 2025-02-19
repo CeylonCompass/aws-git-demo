781155947d18ad65b46ecc343ce919ac25242877
SupunBHerath <supunbherath@gmail.com>
2025-02-19 15:02:53 +0530

    fix: PR review comments for message API

--- src/main/java/com/helixz/awsgitdemo/messages/MessageRepository.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageRepository.java
@@ -1,6 +1,7 @@
 package com.helixz.awsgitdemo.messages;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
  */
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+   @Query(value = "SELECT * FROM message ORDER BY created_date DESC", nativeQuery = true)
    List<Message>findAllByOrderByCreatedDateDesc();
 
 }
