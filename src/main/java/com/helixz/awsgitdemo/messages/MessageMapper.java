c03d619fc70751e27eaa08bb8833c147dd40c0af
chamith kodikara <chamith@helixzsolutions.com>
2025-02-19 16:29:16 +0100

    feat:[HELIXZ-0001] search api impl -basic search impl

--- src/main/java/com/helixz/awsgitdemo/messages/MessageMapper.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageMapper.java
@@ -6,6 +6,8 @@ import org.mapstruct.ReportingPolicy;
 import com.helixz.awsgitdemo.messages.dto.MessageCreateRequest;
 import com.helixz.awsgitdemo.messages.dto.MessageCreateResponse;
 
+import java.util.List;
+
 /**
  * @author Chamith Kodikara
  */
@@ -15,4 +17,6 @@ public interface MessageMapper {
     Message toMessageEntity(MessageCreateRequest request);
 
     MessageCreateResponse toMessageResponse(Message message);
+
+    List<MessageCreateResponse> toMessageCreateResponse(List<Message> content);
 }
