096ad04b28f79f9fd36be4b6eb357d1ce7ce7b8c
Chathur1 <chathuranga.denuwan@helixzsolutions.com>
2025-02-19 17:46:46 +0530

    feat: implement save and get all messages APIs [HELIXZ-0001]

--- src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageService.java
@@ -1,8 +1,12 @@
 package com.helixz.awsgitdemo.messages;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author Chamith Kodikara
@@ -10,5 +14,21 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class MessageService {
+    private final MessageRepository repository;
+    public ResponseEntity<Object> getAllMessages() {
+        List<Message> all = repository.findAll();
+        if (all.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(all);
+    }
+
+    public ResponseEntity<Object> messagePersist(Message message) {
+        if (message != null && !message.getMessage().isEmpty()){
+            return ResponseEntity.ok(repository.save(message));
+        }
+        return ResponseEntity.badRequest().body("Content must not be empty");
+    }
 }
