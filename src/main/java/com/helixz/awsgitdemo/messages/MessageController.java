aca312b2f51ce7e0c3909429a4dbb73567dd565d
BhagYagam <bhagyagmge@gmail.com>
2025-02-20 19:11:29 +0530

    feat : [HELIXZ-0001] implement get and post method for messages

--- src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
+++ src/main/java/com/helixz/awsgitdemo/messages/MessageController.java
@@ -1,10 +1,35 @@
 package com.helixz.awsgitdemo.messages;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Chamith Kodikara
  */
+@CrossOrigin
+@RequestMapping("api/messages")
+@RequiredArgsConstructor
 @RestController
 public class MessageController {
+
+    private final MessageService messageService;
+
+    @GetMapping
+    public List<Message> getAllMessages() {
+        return messageService.getAllMessages();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Message> createMessage(@RequestBody MessageDto content) {
+        if (content.getContent() == null || content.getContent().trim().isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        Message savedMessage = messageService.createMessage(content.getContent());
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedMessage);
+    }
 }
